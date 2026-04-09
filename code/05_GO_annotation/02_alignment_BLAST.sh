d7c939fe7cbe1a16b1b9561e8b5e5b46c8bdf64c
Felipe Collazos <afcollazosr@gmail.com>
2026-04-09 15:58:01 +0200

    Blast against the Xenopus tropicalis Ensembl reference proteome instead of the uniprot version

--- code/05_GO_annotation/02_alignment_BLAST.sh
+++ code/05_GO_annotation/02_alignment_BLAST.sh
@@ -14,20 +14,23 @@ source ../../.env
 
 cd $DIR/code/05_GO_annotation
 
-# 1. Build database if needed
-if [ ! -f xtrop_proteins.fasta.pin ]; then
-    makeblastdb -in xtrop_proteins.fasta -dbtype prot
+# 1. Build Ensembl X. tropicalis protein BLAST database
+if [ ! -f Xenopus_tropicalis.UCB_Xtro_10.0.pep.all.fa.pin ]; then
+    makeblastdb \
+      -in Xenopus_tropicalis.UCB_Xtro_10.0.pep.all.fa \
+      -dbtype prot
 fi
 
-# 2. Run BLAST
+# 2. Run BLAST against Ensembl proteome
 blastp \
   -query proteins.fa \
-  -db xtrop_proteins.fasta \
+  -db Xenopus_tropicalis.UCB_Xtro_10.0.pep.all.fa \
   -outfmt "6 qseqid sseqid pident length evalue bitscore" \
   -evalue 1e-5 \
   -max_target_seqs 5 \
-  -num_threads $SLURM_CPUS_PER_TASK \
-  -out blast_results_${SLURM_JOB_ID}.tsv
+  -num_threads "${SLURM_CPUS_PER_TASK:-1}" \
+  -out "blast_results_${SLURM_JOB_ID}.tsv"
 
-sort -k1,1 -k6,6gr -k5,5g blast_results_${SLURM_JOB_ID}.tsv | \
-awk '!seen[$1]++' > blast_best_${SLURM_JOB_ID}.tsv
+# 3. Keep best hit per query
+sort -k1,1 -k6,6gr -k5,5g "blast_results_${SLURM_JOB_ID}.tsv" | \
+awk '!seen[$1]++' > "blast_best_${SLURM_JOB_ID}.tsv"
